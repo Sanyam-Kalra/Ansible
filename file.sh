ecd92efbec433289f2e541d3d98e3c80a8bcc207
Sanyam Kalra <68919514+Sanyam-Kalra@users.noreply.github.com>
2022-11-05 18:56:23 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
+set -x
+USER=$1
+Key_path=$2
+touch  storefile
+echo "" > storefile
+IP=$(terraform output -json bashinstance_ip | jq -s -r '.[]')
+echo $IP >> storefile
 
+ touch Invnetory 
+ echo "" > Invnetory
+ echo "[seed-nodes]" > Invnetory
+ for line in $(cat storefile)
+ do
+     y=$(echo "${line}") 
+     IP=$(echo $y | awk -F, '{print $1}')
+    echo "node${j} ansible_host=${line} ansible_user=${USER} ansible_ssh_private_key_file=${Key_path} ansible_ssh_common_args='-o StrictHostKeyChecking=no'" >> Invnetory 
+     ((j++))
+     ((i++))
+ done
+ cat Invnetory
+rm storefile
