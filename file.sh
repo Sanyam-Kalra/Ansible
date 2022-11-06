aab5b5de6fae6212a55de7f85a0a33dbc3066ec8
Sanyam Kalra <68919514+Sanyam-Kalra@users.noreply.github.com>
2022-11-06 14:00:16 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -6,13 +6,22 @@ touch  storefile
 echo "" > storefile
 IP=$(terraform output -json private-ip | jq -s -r '.[]')
 echo $IP >> storefile
+ echo "[Head]" > Invnetory
+ for line in $(cat storefile)
+ do
+     y=$(echo "${line}") 
+     IP=$(echo $y | awk -F, '{print $1}')
+    echo "node${j} ansible_host=${line} ansible_user=${USER} ansible_ssh_private_key_file=${Key_path} ansible_ssh_common_args='-o StrictHostKeyChecking=no'" >> Invnetory 
+     ((j++))
+     ((i++))
+ done
 IP=$(terraform output -json private-ip-backend1 | jq -r '.[]')
 echo $IP >> storefile
 IP=$(terraform output -json private-ip-Haproxy_backend2 | jq -r '.[]')
 echo $IP >> storefile
  touch Invnetory 
- echo "" > Invnetory
- echo "[seed-nodes]" > Invnetory
+ #echo "" > Invnetory
+ echo "[Backend]" >> Invnetory
  for line in $(cat storefile)
  do
      y=$(echo "${line}") 
